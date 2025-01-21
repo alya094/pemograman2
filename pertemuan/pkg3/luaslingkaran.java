5c8ef8f1b5cc94e25b7266c3e3b1f3a528a6d9e7
alya094 <alyaef094@gmail.com>
2025-01-21 09:13:41 +0700

    Add files via upload

---
+++ pertemuan/pkg3/luaslingkaran.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan.pkg3;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class luaslingkaran {
+    public static void main(String[] args) {
+        //membuat variabel 
+        double phi = 3.14;
+        
+        
+        //membuat rumus
+        
+        
+        System.out.println("nilai phi lingkaran = " + phi);
+        System.out.println("jari jari lingkaran = " );
+        System.out.println("luas");
+             
+    }
+    
+}
