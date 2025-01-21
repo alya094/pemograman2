1474410bb2a4aa18148c6e9e98b3ce95081f8f71
alya094 <alyaef094@gmail.com>
2025-01-21 09:15:31 +0700

    Add files via upload

---
+++ tugasKeteranganLulus/OOP.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasKeteranganLulus;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class OOP {
+   
+    static void out (String nama){
+        
+        System.out.println(nama);
+        
+    }
+    
+    public static void main(String[] args) {
+        
+         Penilaian p = new Penilaian();
+         
+         out("mamat");
+         System.out.println(p.Keterangan(90));
+    }
+}
---
+++ tugasKeteranganLulus/Penilaian.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasKeteranganLulus;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Penilaian {
+    String Keterangan (int nilai){
+        String hasil;
+        if (nilai > 70){
+            return "lulus";
+        }
+        else {
+            return "tidak lulus";
+        }
+    }
+    
+}
